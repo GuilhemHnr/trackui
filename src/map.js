977c4125ebdc1b9edd984bdfebbfbb965fa063f8
GuilhemHnr <guilhem.hnr@gmail.com>
2022-05-13 11:57:53 +0200

    creation de la table en temps réel

--- src/map.js
+++ src/map.js
@@ -4,6 +4,7 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 //autre fond possible  : http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png
 
 var plane;
+var plane_table = document.getElementById("plane_table");
 
 var myStyle = {
     stroke: true,
@@ -11,6 +12,7 @@ var myStyle = {
     weight: 2,
     opacity: 0.5
 };
+
 var planeIcon = L.icon({
         iconUrl: './ressources/plane.png',
     
@@ -20,6 +22,7 @@ var planeIcon = L.icon({
 
 function onEachFeature(feature, layer) {
     // This is where it loop through your features
+
     var numPts = feature.geometry.coordinates.length;
     layer.setStyle(myStyle);
     if (numPts >= 1) {
@@ -27,11 +30,32 @@ function onEachFeature(feature, layer) {
 
         let mark = new L.Marker([end[1],end[0]], {iconAngle: feature.properties.track});
 
-        console.log(feature.properties.track);
         mark.bindPopup('<h1>'+feature.id+'</h1>');
         mark.setIcon(planeIcon);
         mark.addTo(layerGroup);
     }
+
+    // table
+    var row = plane_table.insertRow();
+    // --- creation of cells
+    var cell_id = row.insertCell();
+    var cell_callsign = row.insertCell();
+    var cell_altitude = row.insertCell();
+    var cell_speed = row.insertCell();
+    var cell_vt_spd = row.insertCell();
+
+    // --- fill cells
+    if (numPts >= 1) {
+        cell_id.innerHTML = '<input id='+feature.id+'_button type="button" value='+feature.id+' onclick = />';
+        var button = document.getElementById(feature.id+'_button');
+        button.onclick = function () {map.flyTo([end[1],end[0]], 8)};
+    } else {cell_id.innerHTML = feature.id;}
+
+    cell_callsign.innerHTML = feature.properties.callsign;
+    cell_altitude.innerHTML = feature.properties.altitude+' ft';
+    cell_speed.innerHTML = Math.trunc(feature.properties.speed)+' kt';
+    cell_vt_spd.innerHTML = feature.properties.sd_vt;
+
 };
 
 var layerGroup = new L.LayerGroup();
@@ -44,8 +68,9 @@ function update_position() {
     $.getJSON("http://127.0.0.1:3000/planes.geojson?t="+curTimeStamp, function(data) {
         console.log(data);
         
-        layerGroup.clearLayers();
+        plane_table.innerHTML="";
         plane = L.geoJSON(data, {onEachFeature: onEachFeature} );
+        layerGroup.clearLayers();
         layerGroup.addLayer(plane);
 
     });
