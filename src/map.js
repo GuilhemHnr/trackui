47c48ac9662a28f2ed64120c027b0e190c9312f7
GuilhemHnr <guilhem.hnr@gmail.com>
2022-05-24 23:08:15 +0200

    licence OSM + CartoDB

--- src/map.js
+++ src/map.js
@@ -1,5 +1,7 @@
 var map = L.map('map',{center: [48.633333, 2.450000],zoom: 8},);
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+}).addTo(map);
 
 //autre fond possible  : http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png
 
